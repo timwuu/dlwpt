3a2c070c0b6c7675353f5350fbca6ca88108042d
timwuu <timwuu@gmail.com>
2019-12-20 21:20:58 +0800

    add new examples

--- p1ch5/3_optimizers.py
+++ p1ch5/3_optimizers.py
@@ -1,8 +1,10 @@
 import numpy as np 
 import torch
+import torch.optim as optim
+
 torch.set_printoptions(edgeitems=2)
 
-t_c = torch.tensor([0.5,  14.0, 15.0, 28.0, 11.0,  8.0,  3.0, -4.0,  6.0, 13.0, 21.0])
+t_c = torch.tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0])
 t_u = torch.tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4])
 t_un = 0.1 * t_u
 
@@ -13,35 +15,30 @@ def loss_fn(t_p, t_c):
     squared_diffs = (t_p - t_c)**2
     return squared_diffs.mean()
 
-# params = torch.tensor([1.0, 0.0], requires_grad=True)
-
-# loss = loss_fn(model(t_u, *params), t_c)
-# loss.backward()
-
-# print( params.grad)
-
-def training_loop(n_epochs, learning_rate, params, t_u, t_c):
+def training_loop(n_epochs, optimizer, params, t_u, t_c):
     for epoch in range(1, n_epochs + 1):
-        if params.grad is not None:  # <1>
-            params.grad.zero_()
-        
         t_p = model(t_u, *params) 
         loss = loss_fn(t_p, t_c)
-        loss.backward()
         
-        params = (params - learning_rate * params.grad).detach().requires_grad_()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
         if epoch % 500 == 0:
             print('Epoch %d, Loss %f' % (epoch, float(loss)))
             
     return params
 
+params = torch.tensor([1.0, 0.0], requires_grad=True)
+learning_rate = 1e-2
+optimizer = optim.SGD([params], lr=learning_rate) # <1>
+
 params= training_loop(
     n_epochs = 5000, 
-    learning_rate = 1e-2, 
-    params = torch.tensor([1.0, 0.0], requires_grad=True), # <1> 
-    t_u = t_un, # <2> 
+    optimizer = optimizer,
+    params = params, # <1> 
+    t_u = t_un,
     t_c = t_c)
 
-print( params)
+print(params)
 
